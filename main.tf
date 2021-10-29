eca285847e9eb2d9612143dce264cbf33f71b0dc
ilopez <ilopez@geocom.com.uy>
2021-10-29 02:28:06 -0300

    update cidr

--- main.tf
+++ main.tf
@@ -17,23 +17,23 @@ provider "aws" {
 }
 
 resource "aws_vpc" "tarea2" {
-  cidr_block = "172.23.0.0/16"
+  cidr_block = "10.0.0.0/16"
   tags = {
     Name = "vpc_iac"
   }
 }
 
 resource "aws_subnet" "web" {
-  cidr_block = "172.23.1.0/20"
+  cidr_block = "10.0.1.0/20"
   vpc_id = aws_vpc.tarea2.id
 }
 
 resource "aws_subnet" "backend" {
-  cidr_block = "172.23.2.0/20"
+  cidr_block = "10.0.2.0/20"
   vpc_id = aws_vpc.tarea2.id
 }
 
 resource "aws_subnet" "db" {
-  cidr_block = "172.23.3.0/20"
+  cidr_block = "10.0.3.0/20"
   vpc_id = aws_vpc.tarea2.id
 }
