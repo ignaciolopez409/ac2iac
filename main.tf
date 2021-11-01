04f4d4984ea6356616d84158a5c7417c9ac4cab2
ilopez <ilopez@geocom.com.uy>
2021-11-01 20:35:20 -0300

    fix main.tf, s3 bucket + id

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "ac2-terraform-states"
+    key    = "iac/terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 resource "aws_vpc" "ac2iac_vpc" {
   cidr_block = var.vpc_cidr
   tags = {
@@ -37,7 +50,7 @@ resource "aws_subnet" "database" {
 }
 
 resource "aws_route_table" "ac2iac_route_table" {
-  vpc_id =  aws_vpc.ac2iac_vpc
+  vpc_id =  aws_vpc.ac2iac_vpc.id
   route {
     cidr_block = var.ac2iac_rt_cidr_block
     gateway_id = aws_internet_gateway.ac2iac_igw.id
--- provider.tf
+++ provider.tf
@@ -1,16 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "ac2-terraform-states"
-    key    = "iac/terraform.tfstate"
-    region = "us-east-1"
-  }
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
